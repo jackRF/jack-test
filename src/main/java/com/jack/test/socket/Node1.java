6084143c5afd04d423d660c96ac2719db42792b8
张伟 <1414211340@qq.com>
2017-12-03 15:26:55 +0800

    组播

---
+++ src/main/java/com/jack/test/socket/Node1.java
@@ -0,0 +1,35 @@
+package com.jack.test.socket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+public class Node1 {
+	private static int port =8000;
+	private static String address="228.0.0.4";
+	public static void main(String[] args) {
+		try {
+			InetAddress group=InetAddress.getByName(address);
+			@SuppressWarnings("resource")
+			MulticastSocket mss=new MulticastSocket(port);
+			mss.joinGroup(group);
+			while(true) {
+				String message="Hello from node1";
+				byte[] buffer=message.getBytes();
+				DatagramPacket dp=new DatagramPacket(buffer, buffer.length,group, port);
+				mss.send(dp);
+				Thread.sleep(1000);
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ src/main/java/com/jack/test/socket/Node2.java
@@ -0,0 +1,32 @@
+package com.jack.test.socket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+public class Node2 {
+	private static int port =8000;
+	private static String address="228.0.0.4";
+	public static void main(String[] args) {
+		try {
+			InetAddress group=InetAddress.getByName(address);
+			MulticastSocket mss=new MulticastSocket(port);
+			mss.joinGroup(group);
+			byte[] buffer=new byte[1024];
+			while(true) {
+				DatagramPacket dp=new DatagramPacket(buffer, buffer.length);
+				mss.receive(dp);
+				String s=new String(dp.getData(),0,dp.getLength());
+				System.out.println("receive from node1:"+s);
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
